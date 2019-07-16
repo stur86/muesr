73e89821cf4f2bff0be748fd234eb9c04064d3cf
Simone Sturniolo <simone.sturniolo@gmail.com>
2019-07-16 13:12:26 +0100

    Added spins/fields/dipole methods

--- muesr/engines/soprano.py
+++ muesr/engines/soprano.py
@@ -5,7 +5,9 @@ orientations
 """
 
 import numpy as np
+import scipy.constants as cnst
 from soprano.utils import minimum_supcell, supcell_gridgen
+from soprano.properties.nmr.utils import _get_isotope_data
 
 
 def find_smallest_scell(sample, radius):
@@ -23,7 +25,8 @@ def find_smallest_scell(sample, radius):
     cell = sample.cell.get_cell()
     return list(minimum_supcell(radius, cell))
 
-def _get_spins(sample, scell):
+
+def _get_spins(sample, scell, momtype='e'):
 
     cell = sample.cell.get_cell()
     fxyz, xyz = supcell_gridgen(cell, scell)
@@ -31,13 +34,62 @@ def _get_spins(sample, scell):
     fpos = sample.cell.get_scaled_positions()
     pos = sample.cell.get_positions()
 
-    sfpos = fpos[None,:,:]+fxyz[:,None,:]
-    spos = pos[None,:,:]+xyz[:,None,:]
+    sfpos = fpos[None, :, :]+fxyz[:, None, :]
+    spos = pos[None, :, :]+xyz[:, None, :]
 
     spins = (spos*0.0j)
 
     k = sample.mm.k
     fc = sample.mm.fc
 
-    ker = np.exp(2.0j*np.pi*np.dot(k, fxyz.T))
-    print(fc[None,:,:]*ker[:,None,:])
+    ker = np.exp(-2.0j*np.pi*np.dot(k, sfpos.T))
+    spins = np.real(fc[None, :, :]*ker[:, None, None])
+
+    # Now turn those into actual dipole moments...
+    if momtype == 'e':
+        g_e = (cnst.physical_constants['Bohr magneton'][0] *
+               cnst.physical_constants['electron g factor'][0])/cnst.hbar
+        gammas = np.repeat([g_e], len(pos))
+    elif momtype == 'n':
+        gammas = _get_isotope_data(sample.cell.get_chemical_symbols(),
+                                   'gamma')
+    else:
+        raise ValueError('Invalid momtype argument passed to _get_spins')
+    gammas = np.repeat(gammas[None, :], len(fxyz), axis=0)
+
+    return spos, spins, gammas
+
+
+def _dipten_all(sample, radius, momtype='e'):
+
+    scell = find_smallest_scell(sample, radius)
+    spos, spins, gammas = _get_spins(sample, scell, momtype)
+
+    # Build the full dipolar tensors
+    dipten = []
+    fields = []
+
+    for mu in sample.muons:
+        mupos = np.dot(sample.cell.get_cell(), mu)
+        r = spos-mupos[None, None]
+        d = np.linalg.norm(r, axis=-1)
+        sph_i = np.where(d <= radius)
+
+        r = r[sph_i]
+        d = d[sph_i]
+        s = spins[sph_i]
+        g = gammas[sph_i]
+
+        # Build the tensors
+        dyad = (3*r[:, :, None]*r[:, None, :] /
+                d[:, None, None]**2-np.eye(3)[None])
+        dyad *= (-cnst.mu_0*cnst.hbar/(8*np.pi*d[:, None, None]**3)
+                 * 1e30*g[:, None, None])
+
+        dipten.append(dyad)
+        fields.append(np.sum(dyad*s[:,:,None], axis=1))
+
+    dipten = np.array(dipten)
+    fields = np.array(fields)
+
+    return dipten, fields
